2c4dd2fd9aa75883a0e1639a06f1f2655ea7c74b
YuqinHu <h878688736@gmail.com>
2023-02-16 20:49:21 -0800

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,14 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (_error, _response, body) => {
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('breed not found');
-    return;
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, _response, body) => {
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(error, data[0].description);
+    } else {
+      callback(null, null)
+    }
+  });
+};
 
+module.exports = { fetchBreedDescription };
